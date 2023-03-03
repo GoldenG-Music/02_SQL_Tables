8bd6d3a1fbb9ba0353fa381bb23fe077a92cf91e
GoldenG-Music <g.avdiu@web.de>
2023-03-03 10:50:01 +0000

    NEW Change File

--- 02a_db_table_SQL.sql
+++ 02a_db_table_SQL.sql
@@ -6,3 +6,10 @@
 
 /* Datenbanken auf Server anzeigen */
 SHOW DATABASES;
+
+/* DB, Löschen wenn vorhanden */
+DROP DATABASE IF EXISTS boo;
+/* DB, Anlegen wenn nicht vorhanden*/
+CREATE DATABASE IF NOT EXISTS boo;
+
+SHOW DATABASES;
